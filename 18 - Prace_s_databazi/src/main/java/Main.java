f17cec60bfcc6c16c5cdf23e4c5d8205b7aeff37
SuchardaTomas <92922742+SuchardaTomas@users.noreply.github.com>
2024-02-14 18:58:31 +0100

    2024-02-14

--- 18 - Prace_s_databazi/src/main/java/Main.java
+++ 18 - Prace_s_databazi/src/main/java/Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
         try {
             DatabaseManager.setData("jmeno", "prijmeni", "email");
             DatabaseManager.getData();
